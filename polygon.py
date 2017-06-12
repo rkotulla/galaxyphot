f4c4b84bdb7342cf779a0c2c526584b0c0199333
Ralf Kotulla <kotulla@wisc.edu>
2017-06-12 14:44:43 -0500

    added support for multiple polygons

--- polygon.py
+++ polygon.py
@@ -28,16 +28,19 @@ def create_mask(img_fn, reg_fn, extname):
     #
     # read region file
     #
+    full_mask = numpy.zeros(data.shape, dtype=numpy.bool)
+
     with open(reg_fn, "r") as reg:
         lines = reg.readlines()
 
-        if (lines[2].strip() == "fk5"):
-            print "#\n"*5,"# USING FK5 coords","\n#"*5
+        for line in lines:
+            if (not line.startswith("polygon")):
+                continue
+            print line
 
-            for line in lines:
-                if (not line.startswith("polygon")):
-                    continue
-                print line
+            if (lines[2].strip() == "fk5"):
+                print "#\n"*5,"# USING FK5 coords","\n#"*5
+
 
                 skycoords = [float(f) for f in
                           line.split("(")[1].split(")")[0].split(",")]
@@ -51,12 +54,7 @@ def create_mask(img_fn, reg_fn, extname):
                 print coords
 
 
-        else:
-            for line in lines:
-                if (not line.startswith("polygon")):
-                    continue
-                print line
-
+            else:
                 coords = [float(f) - 1.5 for f in
                           line.split("(")[1].split(")")[0].split(",")]
                 # print coords
@@ -65,46 +63,46 @@ def create_mask(img_fn, reg_fn, extname):
 
 
 
+            _min = numpy.min(coords, axis=0)
+            _max = numpy.max(coords, axis=0)
+            print "min/max:", _min, _max
+
+            p = matplotlib.path.Path(coords)  # [(0,0), (0, 1), (1, 1), (1, 0)])
+            # print p
+
+            iy, ix = numpy.indices(data.shape)
+            # print ix
+            # print iy
+            i_xy = numpy.append(ix.reshape((-1, 1)), iy.reshape((-1, 1)), axis=1)
+            # print i_xy
+
+            mask = p.contains_points(i_xy)
+            # print mask
+
+            mask_2d = mask.reshape(data.shape)
+
+            inside_mask = data[mask_2d]
+            n_pixels = numpy.sum(mask_2d)
+            print "area:", n_pixels
+
+            # flux = numpy.sum(inside_mask[inside_mask>0])
+            print "flux:", numpy.sum(inside_mask)
+            print "mean:", numpy.median(inside_mask)
+            print "median:", numpy.median(inside_mask)
+
+            print "dx/dy", (_max[1] - _min[1]), (_max[0] - _min[0])
+            print "box-size:", (_max[1] - _min[1]) * (_max[0] - _min[0])
+
+            full_mask = full_mask | mask_2d
+
+    primhdu = pyfits.PrimaryHDU(data=full_mask.astype(numpy.int))
+    primhdu.writeto("mask.fits", clobber=True)
+    # hdulist[0].data = mask_2d.astype(numpy.int)
+    # hdulist.writeto("mask.fits", clobber=True)
 
 
 
-        _min = numpy.min(coords, axis=0)
-        _max = numpy.max(coords, axis=0)
-        print "min/max:", _min, _max
-
-        p = matplotlib.path.Path(coords)  # [(0,0), (0, 1), (1, 1), (1, 0)])
-        # print p
-
-        iy, ix = numpy.indices(data.shape)
-        # print ix
-        # print iy
-        i_xy = numpy.append(ix.reshape((-1, 1)), iy.reshape((-1, 1)), axis=1)
-        # print i_xy
-
-        mask = p.contains_points(i_xy)
-        # print mask
-
-        mask_2d = mask.reshape(data.shape)
-
-        primhdu = pyfits.PrimaryHDU(data=mask_2d.astype(numpy.int))
-        primhdu.writeto("mask.fits", clobber=True)
-        # hdulist[0].data = mask_2d.astype(numpy.int)
-        # hdulist.writeto("mask.fits", clobber=True)
-
-
-        inside_mask = data[mask_2d]
-        n_pixels = numpy.sum(mask_2d)
-        print "area:", n_pixels
-
-        # flux = numpy.sum(inside_mask[inside_mask>0])
-        print "flux:", numpy.sum(inside_mask)
-        print "mean:", numpy.median(inside_mask)
-        print "median:", numpy.median(inside_mask)
-
-        print "dx/dy", (_max[1] - _min[1]), (_max[0] - _min[0])
-        print "box-size:", (_max[1] - _min[1]) * (_max[0] - _min[0])
-
-    return mask_2d
+    return full_mask
 
 if __name__ == "__main__":
 
