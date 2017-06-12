b439cfe7fb35e8aa2ba1080afc68ffac05d4a5bc
Ralf Kotulla <kotulla@wisc.edu>
2017-06-12 14:38:55 -0500

    added support for regions files using FK5 (degrees) coordinates!

--- polygon.py
+++ polygon.py
@@ -7,6 +7,8 @@ import numpy
 import matplotlib
 import matplotlib.path
 
+from astLib import astWCS
+
 # hi lauren
 
 def create_mask(img_fn, reg_fn, extname):
@@ -28,52 +30,79 @@ def create_mask(img_fn, reg_fn, extname):
     #
     with open(reg_fn, "r") as reg:
         lines = reg.readlines()
-        for line in lines:
-            if (not line.startswith("polygon")):
-                continue
-            print line
 
-            coords = [float(f) - 1.5 for f in
-                      line.split("(")[1].split(")")[0].split(",")]
-            # print coords
-            coords = numpy.array(coords).reshape((-1, 2))
-            print coords
+        if (lines[2].strip() == "fk5"):
+            print "#\n"*5,"# USING FK5 coords","\n#"*5
 
-            _min = numpy.min(coords, axis=0)
-            _max = numpy.max(coords, axis=0)
-            print "min/max:", _min, _max
+            for line in lines:
+                if (not line.startswith("polygon")):
+                    continue
+                print line
 
-            p = matplotlib.path.Path(coords)  # [(0,0), (0, 1), (1, 1), (1, 0)])
-            # print p
+                skycoords = [float(f) for f in
+                          line.split("(")[1].split(")")[0].split(",")]
+                # print coords
+                skycoords = numpy.array(skycoords).reshape((-1, 2))
+                print skycoords
 
-            iy, ix = numpy.indices(data.shape)
-            # print ix
-            # print iy
-            i_xy = numpy.append(ix.reshape((-1, 1)), iy.reshape((-1, 1)), axis=1)
-            # print i_xy
-
-            mask = p.contains_points(i_xy)
-            # print mask
-
-            mask_2d = mask.reshape(data.shape)
-
-            primhdu = pyfits.PrimaryHDU(data=mask_2d.astype(numpy.int))
-            primhdu.writeto("mask.fits", clobber=True)
-            # hdulist[0].data = mask_2d.astype(numpy.int)
-            # hdulist.writeto("mask.fits", clobber=True)
+                wcs = astWCS.WCS(hdulist[extname].header, mode='pyfits')
+                xy = wcs.wcs2pix(skycoords[:,0], skycoords[:,1])
+                coords = numpy.array(xy)
+                print coords
 
 
-            inside_mask = data[mask_2d]
-            n_pixels = numpy.sum(mask_2d)
-            print "area:", n_pixels
+        else:
+            for line in lines:
+                if (not line.startswith("polygon")):
+                    continue
+                print line
 
-            # flux = numpy.sum(inside_mask[inside_mask>0])
-            print "flux:", numpy.sum(inside_mask)
-            print "mean:", numpy.median(inside_mask)
-            print "median:", numpy.median(inside_mask)
+                coords = [float(f) - 1.5 for f in
+                          line.split("(")[1].split(")")[0].split(",")]
+                # print coords
+                coords = numpy.array(coords).reshape((-1, 2))
+                print coords
 
-            print "dx/dy", (_max[1] - _min[1]), (_max[0] - _min[0])
-            print "box-size:", (_max[1] - _min[1]) * (_max[0] - _min[0])
+
+
+
+
+
+        _min = numpy.min(coords, axis=0)
+        _max = numpy.max(coords, axis=0)
+        print "min/max:", _min, _max
+
+        p = matplotlib.path.Path(coords)  # [(0,0), (0, 1), (1, 1), (1, 0)])
+        # print p
+
+        iy, ix = numpy.indices(data.shape)
+        # print ix
+        # print iy
+        i_xy = numpy.append(ix.reshape((-1, 1)), iy.reshape((-1, 1)), axis=1)
+        # print i_xy
+
+        mask = p.contains_points(i_xy)
+        # print mask
+
+        mask_2d = mask.reshape(data.shape)
+
+        primhdu = pyfits.PrimaryHDU(data=mask_2d.astype(numpy.int))
+        primhdu.writeto("mask.fits", clobber=True)
+        # hdulist[0].data = mask_2d.astype(numpy.int)
+        # hdulist.writeto("mask.fits", clobber=True)
+
+
+        inside_mask = data[mask_2d]
+        n_pixels = numpy.sum(mask_2d)
+        print "area:", n_pixels
+
+        # flux = numpy.sum(inside_mask[inside_mask>0])
+        print "flux:", numpy.sum(inside_mask)
+        print "mean:", numpy.median(inside_mask)
+        print "median:", numpy.median(inside_mask)
+
+        print "dx/dy", (_max[1] - _min[1]), (_max[0] - _min[0])
+        print "box-size:", (_max[1] - _min[1]) * (_max[0] - _min[0])
 
     return mask_2d
 
